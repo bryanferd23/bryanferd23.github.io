75f63785f9f97c40ceeffc2e7ff1da53002459e8
bryanferd23 <ferdz.waine.mai@gmail.com>
2024-10-20 20:00:27 +0800

    fixed some animation issues

--- script.js
+++ script.js
@@ -78,7 +78,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const toggleMenu = () => {
         navLinks.classList.toggle('open');
         hamburger.classList.toggle('open');
-        links.forEach(link => link.classList.toggle('fade'));
+        links.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
     };
     const hamburger = document.querySelector('.hamburger');
     const navLinks = document.querySelector('.nav-links');
